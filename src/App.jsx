2b64d30e96cb1fd0a301c1dec578bfed8e51caa7
shibsankarBera <shibu900099@gmail.com>
2026-04-09 00:25:33 +0530

    implemented category filtering using dynamic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { BrowserRouter,Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import BrowseBooks from "./pages/BrowseBooks";
 
 function App() {
   return (
@@ -8,15 +9,15 @@ function App() {
       <h1 className="text-3xl font-bold text-blue-600 text-center mt-10">
         Online Library System
       </h1>
-     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />}  />
-        <Route path="/books" element={<h1 className="p-5">Browse Books</h1>} />
-        <Route path="/add-book" element={<h1 className="p-5">Add Book</h1>} />
-      </Routes>
-     </BrowserRouter>
-     
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<BrowseBooks />} />
+          <Route path="/books/:category" element={<BrowseBooks />} />
+          <Route path="/add-book" element={<h1 className="p-5">Add Book</h1>} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
