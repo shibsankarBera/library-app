9a122731ffde626fcfb4186298a0386eea5841bc
shibsankarBera <shibu900099@gmail.com>
2026-04-09 00:15:32 +0530

    added dummy book data

---
+++ src/data/books.js
@@ -0,0 +1,22 @@
+const books = [
+  {
+    id: 1,
+    title: "Dune",
+    author: "Frank Herbert",
+    category: "Sci-Fi",
+  },
+  {
+    id: 2,
+    title: "Sapiens",
+    author: "Yuval Noah Harari",
+    category: "Non-Fiction",
+  },
+  {
+    id: 3,
+    title: "1984",
+    author: "George Orwell",
+    category: "Fiction",
+  },
+];
+
+export default books;
